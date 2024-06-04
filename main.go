3dec99d506504d8df1d26f63085d80ef3296b245
Simon Eason <sie.eason@gmail.com>
2024-06-04 11:28:05 +0800

    Saving generated image to file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"image"
+	"image/png"
+	"os"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
@@ -19,9 +21,11 @@ func main() {
 	// Generate initial image
 	generateImage()
 
+	// Canvas to display current image
 	image := canvas.NewImageFromImage(imgState)
 	image.FillMode = canvas.ImageFillOriginal
 
+	// Button to generate new image
 	generateButton := widget.NewButton("Generate", func() {
 		generateImage()
 		image.Image = imgState
@@ -29,11 +33,27 @@ func main() {
 	})
 	generateButton.Importance = widget.HighImportance
 
+	// Button to save current image
+	saveButton := widget.NewButton("Save", func() {
+		// Save image
+		f, _ := os.Create("image.png")
+		_ = png.Encode(f, imgState)
+	})
+
 	// Create window layout
 	w.SetContent(
-		container.NewVBox(
+		container.NewBorder(
+			nil,
+			container.NewBorder(
+				nil,
+				nil,
+				nil,
+				saveButton,
+				generateButton,
+			),
+			nil,
+			nil,
 			image,
-			generateButton,
 		),
 	)
 
