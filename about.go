d28be0d2dbaa04c278c965883d4313cd21d42a99
Simon Eason <sie.eason@gmail.com>
2024-06-06 11:24:29 +0800

    Added about dialog

--- about.go
+++ about.go
@@ -1,5 +1,27 @@
 package main
 
-func showAbout() {
+import (
+	"fmt"
 
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+func showAbout() {
+	// Create layout
+	// Separate markdown widget for better spacing
+	d := container.NewVBox(
+		widget.NewRichTextFromMarkdown(fmt.Sprintf("Version: **%s**", a.Metadata().Version)),
+		widget.NewRichTextFromMarkdown("Created by: [odddollar (Simon Eason)](https://github.com/odddollar)"),
+		widget.NewRichTextFromMarkdown("Source: [github.com/odddollar/Bilinear-gradient-generator-GUI](https://github.com/odddollar/Bilinear-gradient-generator-GUI)"),
+	)
+
+	// Show information dialog with layout
+	dialog.ShowCustom(
+		"About",
+		"OK",
+		d,
+		mainWindow,
+	)
 }
