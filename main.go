33c33d4b68cee14c93d798a0d8774a5c2c86c1d5
Simon Eason <sie.eason@gmail.com>
2024-06-09 21:10:10 +0800

    Added buttons to corners

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ import (
 // Holds global image state
 var imageCurrent image.Image
 
+// Holds corner pixels
+var topLeftPixel image.NRGBA
+var topRightPixel image.NRGBA
+var bottomLeftPixel image.NRGBA
+var bottomRightPixel image.NRGBA
+
 // Variables to hold widgets
 var a fyne.App
 var mainWindow fyne.Window
@@ -21,6 +27,10 @@ var imageDisplay *canvas.Image
 var aboutButton *widget.Button
 var generateButton *widget.Button
 var saveButton *widget.Button
+var topLeftButton *widget.Button
+var topRightButton *widget.Button
+var bottomLeftButton *widget.Button
+var bottomRightButton *widget.Button
 
 func main() {
 	// Generate initial image
@@ -44,27 +54,44 @@ func main() {
 	// Button to save current image
 	saveButton = widget.NewButton("Save PNG", saveImage)
 
+	// Buttons to change corner pixel values
+	topLeftButton = widget.NewButton("...", func() {})
+	topRightButton = widget.NewButton("...", func() {})
+	bottomLeftButton = widget.NewButton("...", func() {})
+	bottomRightButton = widget.NewButton("...", func() {})
+
 	// Create window layout
-	mainWindow.SetContent(
+	content := container.NewBorder(
 		container.NewBorder(
 			nil,
+			nil,
+			topLeftButton,
+			topRightButton,
+			nil,
+		),
+		container.NewBorder(
 			container.NewBorder(
 				nil,
 				nil,
+				bottomLeftButton,
+				bottomRightButton,
 				nil,
-				container.NewHBox(
-					saveButton,
-					aboutButton,
-				),
-				generateButton,
 			),
 			nil,
 			nil,
-			imageDisplay,
+			container.NewHBox(
+				saveButton,
+				aboutButton,
+			),
+			generateButton,
 		),
+		nil,
+		nil,
+		imageDisplay,
 	)
 
 	// Set window properties and run
+	mainWindow.SetContent(content)
 	//mainWindow.SetFixedSize(true)
 	mainWindow.Show()
 	a.Run()
