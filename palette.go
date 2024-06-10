5c56142befe58c9dd4059a7bbb2f114910bd47a4
Simon Eason <sie.eason@gmail.com>
2024-06-10 19:25:00 +0800

    Updating corner value with colour picker

--- palette.go
+++ palette.go
@@ -9,7 +9,11 @@ import (
 func pickColour(cornerColour *color.NRGBA) {
 	// Open advanced colour picker dialog and set initial colour to current one
 	d := dialog.NewColorPicker("Colour", "Pick corner colour", func(c color.Color) {
+		// Convert color.Color to color.NRGBA
+		(*cornerColour) = color.NRGBAModel.Convert(c).(color.NRGBA)
 
+		// Refresh image with new corner value
+		refreshImage()
 	}, mainWindow)
 	d.Advanced = true
 	d.SetColor(cornerColour)
