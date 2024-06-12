23cf72e305fb83285e5ceb67ea9536640ea9e351
Simon Eason <sie.eason@gmail.com>
2024-06-12 15:15:57 +0800

    Added widgets to options dialog

--- options.go
+++ options.go
@@ -1,18 +1,45 @@
 package main
 
-import "fyne.io/fyne/v2/dialog"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/data/validation"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
 
 // Show options in dialog
 func showOptions() {
+	// Width and height entry boxes
+	widthEntry := widget.NewEntry()
+	widthEntry.Validator = validation.NewRegexp(`^[3-9]|[0-9]{2,}$`, "Must be greater than 2")
+	heightEntry := widget.NewEntry()
+	heightEntry.Validator = validation.NewRegexp(`^[3-9]|[0-9]{2,}$`, "Must be greater than 2")
+
+	// Randomise alpha checkbox
+	alphaCheckbox := widget.NewCheck("", func(b bool) {})
+
+	// Hide corner button checkbox
+	hideCorners := widget.NewCheck("", func(b bool) {})
+
+	// Create options layout
+	options := []*widget.FormItem{
+		{Text: "Image width", Widget: widthEntry, HintText: "In pixels"},
+		{Text: "Image height", Widget: heightEntry, HintText: "In pixels"},
+		{Text: "Randomise alpha", Widget: alphaCheckbox},
+		{Text: "Hide corner buttons", Widget: hideCorners},
+	}
+
 	// Create new dialog using form items
-	dialog.ShowForm(
+	d := dialog.NewForm(
 		"Options",
 		"Save",
 		"Cancel",
-		nil,
+		options,
 		func(b bool) {
 
 		},
 		mainWindow,
 	)
+	d.Resize(fyne.NewSize(350, 315))
+	d.Show()
 }
